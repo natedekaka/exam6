25ef690c0b16ba298e51b4de034b0a5e62add4f0
natedekaka <natedekaka@gmail.com>
2026-05-13 04:59:20 +0700

    chore: hapus one-time migration/fix scripts yang tidak dipakai

--- alter_jawaban_sEMENTARA.sql
+++
@@ -1,4 +0,0 @@
--- Add IP Address and Device Fingerprint columns to jawaban_sEMENTARA table
-ALTER TABLE `jawaban_sEMENTARA` 
-ADD COLUMN `ip_address` varchar(45) DEFAULT NULL AFTER `updated_at`,
-ADD COLUMN `device_fingerprint` varchar(64) DEFAULT NULL AFTER `ip_address`;
--- fix_violations_table.sql
+++
@@ -1,3 +0,0 @@
--- Fix exam_violations table to add AUTO_INCREMENT to id column
-ALTER TABLE exam_violations 
-MODIFY COLUMN `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY;
